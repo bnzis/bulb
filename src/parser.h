f29df7a1e582231183fdab5dec2976934e58e33a
bnzis <bonzisoft@protonmail.com>
2018-01-15 10:21:40 +0100

    [DOCS] Add some comments
    Useless to say that it's really important to comment the code.
    The comments added inside the headers will be taken by an external script
    and used for generating documentation files in markdown.

--- src/parser.h
+++ src/parser.h
@@ -22,16 +22,26 @@
 #include <string.h>
 #include "types.h"
 
+/* Acquires the AST from string */
 obj_t *parse(char *exp);
 
+/* Verifies if said string can be converted to an integer (doesn't accept 
+ * decimals). */
 bool is_int(char *exp, unsigned len);
 
+/* Verifies if the string can be converted to a float (decimal part is 
+ * needed). */
 bool is_float(char *exp, unsigned len);
 
+/* Converts the string to an object. */
 obj_t atom(char *exp, unsigned len);
 
+/* Returns the type of the token contained in said string starting from its 
+ * offset. This only works if the conversion is possible, therefore if it's not 
+ * a parentesis, gets retured. */
 unsigned get_token(char *exp, unsigned len, unsigned *offset, obj_t *out);
 
+/* Generates an AST starting from the provided offset. */
 obj_t *generate_ast(char *exp, unsigned len, unsigned *offset);
 
 #endif
