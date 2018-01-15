450377ddf3d5750ef46c17b9adba1308b37d64f8
bnzis <bonzisoft@protonmail.com>
2018-01-15 10:24:13 +0100

    Add some comments

--- src/hashmap.h
+++ src/hashmap.h
@@ -23,14 +23,25 @@
 #include "types.h"
 #include "xxhash.h"
 
+/* Calculates the hash. */
 unsigned long long hash(const void* buffer, int length);
 
+/* Adds the object to the hashmap associating it with the key. NOTE: it doesn't 
+ * check if there's already a stored key. It's also generally faster than 
+ * set(). */
 void append(hashmap_t *map, char *key, obj_t *obj);
 
+/* Returns the object associated to said key, if the object is not available 
+ * NULL is returned. */
 obj_t *get(hashmap_t *map, char *key);
 
+/* Associates the key to its object inside the hashmap, verifies if there's 
+ * already a key and if so, it updates it. NOTE: the pointer is updated, but the
+ * object isn't deallocated. Generally slower than append(), but safer. */
 void set(hashmap_t *map, char *key, obj_t *obj);
 
+/* Deletes the key and the associated object from the hashmap.
+   NOTE: the pointers are removed, while key and object aren't touched. */
 void delete(hashmap_t *map, char *key);
 
 #endif 
