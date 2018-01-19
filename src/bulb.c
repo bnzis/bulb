dff63ddbc2b81d8f240c053af5ec8900bccb209f
bnzis <bonzisoft@protonmail.com>
2018-01-19 09:50:06 +0100

    Progress

--- src/bulb.c
+++ src/bulb.c
@@ -19,31 +19,16 @@
 #include <stdio.h>
 #include "bulb.h"
 
-void print_ast(obj_t *tree)
-{
-    if (tree) {
-        if (tree->type == SYMBOL || tree->type == STRING)
-            printf("%s\n", tree->data.symbol.buff);
-        else if (tree->type == INT)
-            printf("%i\n", tree->data.integer);
-        else if (tree->type == FLOAT)
-            printf("%f\n", tree->data.floating);
-        else if(tree->type == CONS) {
-            print_ast(tree->data.cons.car);
-            print_ast(tree->data.cons.cdr);
-        }
-    }
-}
-
 int main() 
 {
-    char *program = "(+ 10 20)";
+    char *program = "(def a (+ 1 2)) (+ a 20)";
     obj_t *tree = parse(program);
     env_t *env = malloc(sizeof(env_t));
     env->local = malloc(sizeof(hashmap_t));
     env->local->data = malloc(sizeof(obj_t*) * HMAP_ROWS);
     obj_t module = atom("core", strlen("core"));
-    load_module(env, &module);
-    printf("%d\n", eval_sequence(tree, env)->data.integer);
+    load_module(&module, env);
+    print_ast(eval_sequence(tree, env));
+    printf("\n");
     return 0;
 }
