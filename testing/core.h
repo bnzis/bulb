46cfe380cddc9dc9a3b53d99caadeed7553b3e21
bnzis <bonzisoft@protonmail.com>
2018-02-06 16:34:25 +0100

    New structure.

--- testing/core.h
+++ testing/core.h
@@ -1,8 +1,8 @@
 /*
  *  Bulb - the Lisp Interpreter
  *  Copyright (C) 2018-2019 bnzis (bonzisoft@protonmail.com)
- *  Copyright (C) 2012-2016 Yann Collet (xxhash)
- *
+ *  Copyright (C) 2012-2016, Yann Collet (xxhash)
+ *  
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -16,11 +16,71 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#ifndef BULB_UTIL_H
-#define BULB_UTIL_H
+#ifndef BULB_CORE_H
+#define BULB_CORE_H
+#include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include "types.h"
-#include "parser.h"
+
+/* token types */
+enum {
+    BULB_TOK_NIL = 0, 
+    BULB_TOK_OPEN_BLOCK, 
+    BULB_TOK_CLOSE_BLOCK, 
+    BULB_TOK_OTHER, 
+};
+
+typedef struct bulbObj_s {
+    void (*type)(struct bulbObj_s *o);
+    void *data;
+} bulbObj;
+
+/* base types */
+typedef void (*bulbType)(bulbObj *o);
+extern bulbType BULB_NIL, BULB_BOOL, BULB_INT, BULB_FLOAT, BULB_SYMBOL, 
+        BULB_STRING, BULB_PROCEDURE, BULB_PRIMITIVE;
+extern bulbType BULB_CONS;
+
+void bulbPrintNil(bulbObj *o), bulbPrintBool(bulbObj *o), 
+        bulbPrintInt(bulbObj *o), bulbPrintFloat(bulbObj *o), 
+        bulbPrintSymbol(bulbObj *o), bulbPrintString(bulbObj *o), 
+        bulbPrintProcedure(bulbObj *o), bulbPrintPrimitive(bulbObj *o), 
+        bulbPrintCons(bulbObj *o);
+
+extern bulbObj *bulbNil, *bulbTrue, *bulbFalse; 
+
+/* HASHMAP SECTION */
+#define HMAP_ROWS 64 
+
+typedef struct {
+    bulbObj **data;
+} bulbHashmap;
+
+typedef struct env_s {
+    bulbHashmap *local;
+    struct env_s *upper_level;
+} bulbEnv;
+
+typedef struct {
+   bulbObj *car;
+   bulbObj *cdr;
+} bulbCons;
+
+typedef struct {
+    bulbObj *args;
+    bulbObj *body;
+    bulbEnv *env;
+} bulbProc;
+
+typedef struct {
+    char *data;
+    unsigned len;
+} bulbSymbol;
+
+typedef struct {
+    char *data;
+    unsigned len;
+} bulbString;
 
 /* Checks if the object is an atom type. */ 
 bool isAtom(bulbObj *obj);
@@ -80,4 +140,4 @@ void bulb_err_not_a_pair(bulbObj *obj);
 
 void bulb_err_out_of_bounds(unsigned index, unsigned len);
 
-#endif /* UTIL_H */
+#endif /* BULB_CORE_H */
